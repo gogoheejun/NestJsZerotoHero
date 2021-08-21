bb3b43e2e2c1705fa99ce2284e204303b7ffc2fc
heejun <heejjuunn@gmail.com>
2021-08-21 13:20:17 +0900

    status enum validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -42,8 +43,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id:string,
-    @Body('status') status:TaskStatus,
+    @Body() updateTaskStatus:UpdateTaskStatusDto,
   ):Task{
+    const {status} = updateTaskStatus
     return this.tasksService.updateTaskStatus(id,status);
   }
 
