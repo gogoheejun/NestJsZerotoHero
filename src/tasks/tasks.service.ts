6fb1863b067ec4bbe1c7c9f86a79c03328585db3
heejun <heejjuunn@gmail.com>
2021-08-21 13:07:41 +0900

    id관련validation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {Task, TaskStatus} from './task.model';
 import {v4 as uuid} from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -38,7 +38,13 @@ export class TasksService {
   }
 
   getTaskById(id:string): Task{
-    return this.tasks.find((task)=>task.id === id);
+    const found = this.tasks.find((task)=>task.id === id);
+
+    if(!found){
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto:CreateTaskDto):Task {
@@ -57,7 +63,8 @@ export class TasksService {
   }
 
   deleteTask(id:string){
-    this.tasks = this.tasks.filter((task)=>task.id !==id);
+    const found = this.getTaskById(id); //벨리데이션
+    this.tasks = this.tasks.filter((task)=>task.id !== found.id);
   }
 
   updateTaskStatus(id:string, status:TaskStatus){
