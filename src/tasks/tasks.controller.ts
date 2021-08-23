1c24acc89a17cce7d43775cb0413e0fe6eb5c83f
heejun <heejjuunn@gmail.com>
2021-08-23 12:48:45 +0900

    리팩토링커밋다시

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,50 +3,50 @@ import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService){}
 
-  //http://localhost:3000/tasks
-  @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto):Task[]{
-    //if we have any filters defined, call tasksService.getTasksWithFilters
-    //otherwise, just get all tasks
-    if(Object.keys(filterDto).length){
-      return this.tasksService.getTasksWithFilters(filterDto);
-    }else{
-      return this.tasksService.getAllTasks();
-    }
+  // //http://localhost:3000/tasks
+  // @Get()
+  // getTasks(@Query() filterDto: GetTasksFilterDto):Task[]{
+  //   //if we have any filters defined, call tasksService.getTasksWithFilters
+  //   //otherwise, just get all tasks
+  //   if(Object.keys(filterDto).length){
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   }else{
+  //     return this.tasksService.getAllTasks();
+  //   }
 
-    return this.tasksService.getAllTasks();
-  }
+  //   return this.tasksService.getAllTasks();
+  // }
 
-  //http://localhost:3000/tasks/alex
-  @Get('/:id')
-  getTaskById(@Param('id') id: string):Task{ //파라미터를 id에다가 담음
-    return this.tasksService.getTaskById(id);
-  }
+  // //http://localhost:3000/tasks/alex
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string):Task{ //파라미터를 id에다가 담음
+  //   return this.tasksService.getTaskById(id);
+  // }
 
-  @Post()
-  createTask(@Body() CreateTaskDto:CreateTaskDto):Task{ //Body를 CreateTaskDto에다가 담음
-    return this.tasksService.createTask(CreateTaskDto);
-  }
+  // @Post()
+  // createTask(@Body() CreateTaskDto:CreateTaskDto):Task{ //Body를 CreateTaskDto에다가 담음
+  //   return this.tasksService.createTask(CreateTaskDto);
+  // }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string):void{
-    return this.tasksService.deleteTask(id);
-  }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string):void{
+  //   return this.tasksService.deleteTask(id);
+  // }
 
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id:string,
-    @Body() updateTaskStatus:UpdateTaskStatusDto,
-  ):Task{
-    const {status} = updateTaskStatus
-    return this.tasksService.updateTaskStatus(id,status);
-  }
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id:string,
+  //   @Body() updateTaskStatus:UpdateTaskStatusDto,
+  // ):Task{
+  //   const {status} = updateTaskStatus
+  //   return this.tasksService.updateTaskStatus(id,status);
+  // }
 
 }
