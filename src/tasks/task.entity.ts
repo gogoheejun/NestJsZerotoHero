34bb87e1f15c08f847d0b708c92312e45346c52d
heejun <heejjuunn@gmail.com>
2021-08-23 12:26:50 +0900

    typeORM추가

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './task.model';
+
+@Entity()
+export class Task{
+  @PrimaryGeneratedColumn('uuid')//uuid안하면 기본으론 123순서임
+  id: string;
+
+  @Column()
+  title:string;
+
+  @Column()
+  description:string;
+
+  @Column()
+  status:TaskStatus;
+}
