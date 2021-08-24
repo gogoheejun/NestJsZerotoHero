d14b8b9d5a79f039d33ff699079e01f464456095
heejun <heejjuunn@gmail.com>
2021-08-24 16:08:37 +0900

    signup with hash

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,13 +2,17 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcryprt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRePository extends Repository<User>{
   async createUser(authCredentialsDto: AuthCredentialsDto):Promise<void>{
     const {username, password} = authCredentialsDto;
 
-    const user = this.create({username, password});
+    //hash
+    const salt = await bcryprt.genSalt();
+    const hashedPassword = await bcryprt.hash(password,salt);
+    const user = this.create({username, password:hashedPassword});
     try{
       await this.save(user);
     }catch(error){
